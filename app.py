e29b986b548c9f63e27ea3ecfa212cc456c452ea
Empya1 <161871238+Empya1@users.noreply.github.com>
2024-05-13 01:23:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -50,13 +50,14 @@ def clear_chat():
 def process(data):
 	
 	chat_text = str(data)
-	time = datetime.datetime.now()
-	m_time = time.strftime("%H : %M")
-	print("chatting")
-	db.session.add(Chat(content=chat_text, time =str(m_time) ))
-	db.session.commit()
-	print("Chat added to db")
-	x = Chat.query.filter(Chat.content==chat_text).first()
-	print(x)
-	socket.emit("show", {"chattext": chat_text, "time": m_time})
+	if chat.text != "":
+		time = datetime.datetime.now()
+		m_time = time.strftime("%H : %M")
+		print("chatting")
+		db.session.add(Chat(content=chat_text, time =str(m_time) ))
+		db.session.commit()
+		print("Chat added to db")
+		x = Chat.query.filter(Chat.content==chat_text).first()
+		print(x)
+		socket.emit("show", {"chattext": chat_text, "time": m_time})
 	
